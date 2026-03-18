63b760946a840a64cb907e24ca67a8b4526e4445
istip-in <jaredsteph08@gmail.com>
2026-03-18 17:26:53 +0800

    Add files via upload

---
+++ RESEARCH OUTPUT/app.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+if (menu && menuLinks) {
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    });
+}
